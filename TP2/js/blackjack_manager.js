280dd0321e86f8cfdf8f36c38da591cff43b6230
David Santos <daapsyt@gmail.com>
2025-10-22 18:32:43 +0100

    jQuery implementation and animations
    FINISH JQUERY

--- TP2/js/blackjack_manager.js
+++ TP2/js/blackjack_manager.js
@@ -1,6 +1,14 @@
 // Blackjack OOP
 let game = null; // Stores the current instance of the game
 
+const CARD_DRAW_DELAY = 250;
+
+$(document).ready(function () {
+  $("#hit").click(playerNewCard);
+  $("#stand").click(dealerFinish);
+  $("#new_game").click(newGame);
+});
+
 /**
  * Function to debug and display the state of the game object.
  * @param {Object} obj - The object to be debugged.
@@ -13,7 +21,7 @@ function debug(obj) {
  * Initializes the game buttons.
  */
 function buttonsInitialization() {
-  document.getElementById("card").disabled = false; // Enables the button to draw a card
+  document.getElementById("hit").disabled = false; // Enables the button to draw a card
   document.getElementById("stand").disabled = false; // Enables the button to stand
   document.getElementById("new_game").disabled = true; // Disables the button for a new game
 }
@@ -22,14 +30,11 @@ function buttonsInitialization() {
  * Finalizes the buttons after the game ends.
  */
 function finalizeButtons() {
-  //TODO: Reveal the dealer's hidden card if you hid it like you were supposed to.
-
-  document.getElementById("card").disabled = true; // Disables the button to draw a card
+  document.getElementById("hit").disabled = true; // Disables the button to draw a card
   document.getElementById("stand").disabled = true; // Disables the button to stand
   document.getElementById("new_game").disabled = false; // Enables the button for a new game
 }
 
-//TODO: Implement this method.
 /**
  * Clears the page to start a new game.
  */
@@ -39,7 +44,6 @@ function clearPage() {
   updateScore(0, 0);
 }
 
-//TODO: Complete this method.
 /**
  * Starts a new game of Blackjack.
  */
@@ -53,31 +57,44 @@ function newGame() {
 }
 
 function initGame() {
-  // TURN SECOND CARD UPSIDE DOWN
   dealerNewCard();
-  playerNewCard();
-  dealerNewCard();
-  playerNewCard();
-  game.getDealerCards()[1].setUpsideDown(true);
-  updateDealer(game.getGameState());
+
+  setTimeout(() => {
+    playerNewCard();
+
+    setTimeout(() => {
+      dealerNewCard();
+
+      setTimeout(() => {
+        playerNewCard();
+
+        game.getDealerCards()[1].setUpsideDown(true);
+        updateDealer(game.getGameState());
+      }, CARD_DRAW_DELAY); // Delay before player's second card
+    }, CARD_DRAW_DELAY); // Delay before dealer's second card
+  }, CARD_DRAW_DELAY); // Delay before player's first card
 }
 
-//TODO: Implement this method.
 /**
  * Calculates and displays the final score of the game.
  * @param {Object} state - The current state of the game.
  */
 function finalScore(state) {
-  if (state.playerWon) alert("PLAYER WINS");
-  else if (state.dealerWon) alert("DEALER WINS");
-  else if (state.playerBusted) alert("PLAYER BUST");
-  else if (state.dealerBusted) alert("DEALER BUST")
-  else alert("PUSH");
-  
+  const modal = new bootstrap.Modal($("#gameResultModal"));
+  let result = "";
+
+  if (state.playerWon) result = "PLAYER WINS";
+  else if (state.dealerWon) result = "DEALER WINS";
+  else if (state.playerBusted) result = "PLAYER BUST";
+  else if (state.dealerBusted) result = "DEALER BUST";
+  else result = "PUSH";
+
+  $("#end-message").text(result);
+
   finalizeButtons();
+  modal.show();
 }
 
-//TODO: Implement this method.
 /**
  * Updates the dealer's state in the game.
  * @param {Object} state - The current state of the game.
@@ -95,7 +112,6 @@ function updateDealer(state) {
   if (state.gameEnded) finalScore(game.getGameState());
 }
 
-//TODO: Implement this method.
 /**
  * Updates the player's state in the game.
  * @param {Object} state - The current state of the game.
@@ -105,11 +121,13 @@ function updatePlayer(state) {
 
   clearElement("player");
   for (let playerCard of game.getPlayerCards()) {
-    printCard(playerElement, playerCard, false).style.transform =
-      "rotate(-5deg)";
+    printCard(playerElement, playerCard, false);
   }
 
-  if (game.getPlayerPoints() === Blackjack.MAX_POINTS || game.state.playerBusted) {
+  if (
+    game.getPlayerPoints() === Blackjack.MAX_POINTS ||
+    game.state.playerBusted
+  ) {
     game.setDealerTurn(true);
     dealerFinish();
   }
@@ -120,40 +138,72 @@ function updatePlayer(state) {
   return state;
 }
 
-//TODO: Implement this method.
 /**
  * Causes the dealer to draw a new card.
  * @returns {Object} - The game state after the dealer's move.
  */
 function dealerNewCard() {
   game.dealerMove();
-
   updateDealer(game.getGameState());
+
+  const $newCard = $("#dealer img:last-child");
+
+  $newCard.css("left", "300px");
+  $newCard.css("bottom", "800px");
+
+  $newCard.animate(
+    {
+      left: "0px",
+      bottom: "0px",
+    },
+    300
+  );
+
   return game.getGameState();
 }
 
-//TODO: Implement this method.
 /**
  * Causes the player to draw a new card.
  * @returns {Object} - The game state after the player's move.
  */
 function playerNewCard() {
   game.playerMove();
-
   updatePlayer(game.getGameState());
+
+  const $newCard = $("#player img:last-child");
+
+  $newCard.css("left", "300px");
+  $newCard.css("bottom", "800px");
+
+  $newCard.animate(
+    {
+      left: "0px",
+      bottom: "0px",
+    },
+    350
+  );
+
   return game.getGameState();
 }
 
-//TODO: Implement this method.
 /**
  * Finishes the dealer's turn.
  */
 function dealerFinish() {
   game.getDealerCards()[1].setUpsideDown(false);
-  while (
+
+  dealerDrawSequence();
+}
+
+function dealerDrawSequence() {
+  if (
     game.getCardsValue(game.getDealerCards()) < Blackjack.DEALER_MAX_TURN_POINTS
   ) {
-    dealerNewCard();
+    setTimeout(() => {
+      dealerNewCard();
+
+      dealerDrawSequence();
+    }, CARD_DRAW_DELAY);
   }
 }
 
@@ -169,9 +219,9 @@ function printCard(element, card, replace = false) {
   cardImage.alt = `${card.getRank()} of ${card.getSuit()}`;
   cardImage.style.width = "150px";
   cardImage.style.marginRight = "-100px";
+  cardImage.style.position = "relative";
 
   element.appendChild(cardImage);
-  return cardImage;
 }
 
 function clearElement(elementId) {
